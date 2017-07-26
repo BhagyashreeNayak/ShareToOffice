e0e584aa0a84504e1ab22266187bd4a1eb65c1d0
BhagyashreeNayak <bhagya.shree.bn@gmail.com>
2017-07-26 16:19:56 +0530

    Launch Activity changes. Change from transparent and add text in center

--- app/src/main/java/com/bhnayak/sharetooffice/LaunchActivity.java
+++ app/src/main/java/com/bhnayak/sharetooffice/LaunchActivity.java
@@ -3,15 +3,20 @@ package com.bhnayak.sharetooffice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LaunchActivity extends AppCompatActivity {
 
     static final int NEW_NOTE_REQUEST = 1;  // The request code
-
+    private TextView mLaunchText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        mLaunchText = (TextView)findViewById(R.id.launch_textview);
+        mLaunchText.setText("Welcome to Office Note! \n" +
+                "Share any text or image with Office Note and it will be available in Office apps on all your devices! ");
 
         Intent intent = getIntent();
         String action = intent.getAction();
